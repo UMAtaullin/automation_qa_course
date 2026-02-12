a1d6253a4a3e116512e0f88bcf1d1dd890b54bf7
UMAtaullin <ataullinural@gmail.com>
2026-02-12 15:43:18 +0500

    added addopts for pytest

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -4,5 +4,4 @@ from pages.base_page import BasePage
 def test(driver):
     page = BasePage(driver, "https://www.google.com")
     page.open()
-    driver.implicitly_wait(10)
-    time.sleep(5)
+    time.sleep(3)
